67ce41b187b5f3e8d967b60253a22267518761f9
Santiago Mendez Ferrin <85143067+mzkrantz@users.noreply.github.com>
2023-06-26 03:10:36 -0300

    Update ControladorBdStreaming.java
    Se agregan varias funcionalidades al controlador.
    Se corrigen bugs
    Se cambia lógica de algunos metodos

--- src/controlador/ControladorBdStreaming.java
+++ src/controlador/ControladorBdStreaming.java
@@ -1,13 +1,17 @@
 package controlador;
 
 import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 import modelo.baseDeDatos.BdStreaming;
 import modelo.baseDeDatos.LimiteClasesException;
-import modelo.baseDeDatos.RevisarAca;
 import modelo.sedes.Actividad;
 import modelo.sedes.Clase;
+import vista.BdStreaming.VistaBdStreamingUser;
+import vista.BdStreaming.VistaBdStreamingAdmin;
+import vista.BdStreaming.VistaBdStreamingActividades;
 
 public class ControladorBdStreaming {
 	private BdStreaming bdStreaming;
@@ -18,6 +22,10 @@ public class ControladorBdStreaming {
 
 	public void agregarClase(Clase clase) {
 		try {
+			int limiteActual = bdStreaming.buscarLimite(clase.getActividad());
+			if(limiteActual == -1) {
+				bdStreaming.cambiarLimite(clase.getActividad(), 5); //Se establece un limite por defecto
+			}
 			bdStreaming.agregarClase(clase);
 			System.out.println("Clase agregada exitosamente a la Base de Datos de Streaming.");
 		} catch (LimiteClasesException e) {
@@ -37,7 +45,7 @@ public class ControladorBdStreaming {
 		bdStreaming.definirLimitePorTipo(actividad, limite);
 	}
 
-	public void cambiarLimite(Actividad actividad, int limite) {
+	public void cambiarLimite(Actividad actividad, int limite) throws LimiteClasesException {
 		bdStreaming.cambiarLimite(actividad, limite);
 	}
 
@@ -57,10 +65,38 @@ public class ControladorBdStreaming {
 		return bdStreaming.getLimitePorActividad();
 	}
 
-	@RevisarAca
-	public List<Actividad> getActividades() {
-		// hay que revisar esto
-		return bdStreaming.getActividades();
+	public List<Clase> filtrarClasesPorId(String filtroId) {
+		List<Clase> clases = bdStreaming.buscarClasePorId(filtroId);
+		return clases;
+	}
+
+	public LocalDate fechaStrAFecha(String stringDate) {
+        LocalDate date = null;
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        date = LocalDate.parse(stringDate, formatter);
+
+        return date;
+	}
+
+	public List<Clase> filtrarClasesPorFecha(LocalDate fechaDesde, LocalDate fechaHasta) throws Exception{
+		List<Clase> clases = bdStreaming.buscarClasesPorFecha(fechaDesde, fechaHasta);
+		return clases;
+	}
+	
+	public void abrirVistaBdStreamingUser() {
+		VistaBdStreamingUser interfaz = new VistaBdStreamingUser();
+		interfaz.setVisible(true);
+	}
+	
+	public void abrirVistaBdStreamingAdmin() {
+		VistaBdStreamingAdmin interfaz = new VistaBdStreamingAdmin();
+		interfaz.setVisible(true);
+	}
+	
+	public void abrirVistaBdStreamingActividades() {
+		VistaBdStreamingActividades interfaz = new VistaBdStreamingActividades();
+		interfaz.setVisible(true);
 	}
 
 }
