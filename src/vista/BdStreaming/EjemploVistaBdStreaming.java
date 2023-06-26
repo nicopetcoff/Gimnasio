fcd8a687a315f806e454a95aa1fdacb3cd819a45
Santiago Mendez Ferrin <safemendez@gmail.com>
2023-06-26 03:04:19 -0300

    Revert "Borrar"
    This reverts commit eac856fe9e5b16e9d0de8c4d37b178fbe1c68dca.

---
+++ src/vista/BdStreaming/EjemploVistaBdStreaming.java
@@ -0,0 +1,12 @@
+package vista.BdStreaming;
+
+public class EjemploVistaBdStreaming {
+	public static void main(String[] args) {
+		VistaBdSUser vista1 = new VistaBdSUser();
+		vista1.setVisible(true);
+
+		VistaBdSAdmin vista2 = new VistaBdSAdmin();
+		vista2.setVisible(true);
+
+	}
+}
