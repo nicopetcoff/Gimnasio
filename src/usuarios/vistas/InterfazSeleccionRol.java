ae3fd97fc82855041710a15da1172895dce481e0
nicolaspetcoff <nicolaspetcoff@localhost>
2023-06-29 09:26:14 -0300

    correcion

--- src/usuarios/vistas/InterfazSeleccionRol.java
+++ src/usuarios/vistas/InterfazSeleccionRol.java
@@ -10,22 +10,22 @@ import java.awt.event.ActionListener;
 
 public class InterfazSeleccionRol extends JFrame {
     public InterfazSeleccionRol() {
-        // Configuración de la ventana principal
+        
         setTitle("Selección de Rol");
         setSize(300, 200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         
-        // Crear los elementos de la interfaz
+
         JLabel label = new JLabel("Selecciona tu rol:");
         JButton clienteButton = new JButton("Cliente");
         JButton administrativoButton = new JButton("Administrativo");
         JButton soporteButton = new JButton("Soporte Técnico");
         
-        // Configurar el diseño de la interfaz
+
         setLayout(new BorderLayout());
         
-        // Agregar los elementos a la ventana
+
         add(label, BorderLayout.NORTH);
         
         JPanel panelBotones = new JPanel();
@@ -35,24 +35,24 @@ public class InterfazSeleccionRol extends JFrame {
         panelBotones.add(soporteButton);
         add(panelBotones, BorderLayout.CENTER);
         
-        // Configurar acciones para los botones
+
         clienteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Acción para el rol de Cliente
+
                 abrirVentanaCliente();
             }
         });
         
         administrativoButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Acción para el rol de Administrativo
+          
                 abrirVentanaAdministrativo();
             }
         });
         
         soporteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Acción para el rol de Soporte Técnico
+
                 abrirVentanaSoporteTecnico();
             }
         });
